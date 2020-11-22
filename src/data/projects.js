4c7769eb637b2e37b13248edef55a451670ad70a
Steve Maguire <dannyblack42@protonmail.com>
2020-11-22 23:37:23 +0000

    add calculator/punk/hero urls

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,13 +19,13 @@ const projects = [
     title: "Punk API",
     image: "https://i.imgur.com/xpCfTRR.png",
     body:
-      "An app to fetch beers from the Punk API. The list of beers can then be filtered according to strength or searched dynamically using a keyword",
+      "An app to fetch beers from the Punk API. The list of beers can then be filtered according to strength or searched dynamically using a keyword. Currently built for desktop view, with breakpoints for smaller screens to be added going forward.",
     github: "https://github.com/Zdeevo/punk-beers",
-    weblink: "https://coming-soon-79bfd.web.app",
+    weblink: "https://punk-beers-c2d48.web.app/",
   },
   {
     title: "Kanye Quote Machine",
-    image: "https://i.imgur.com/2o1RvbS.png",
+    image: "https://via.placeholder.com/150",
     body:
       "Have a chuckle at some of Kanye West's best and most ridiculous quotes, fetched from the Kanye.Rest API.",
     github: "https://github.com/Zdeevo",
@@ -40,12 +40,12 @@ const projects = [
     weblink: "https://coming-soon-79bfd.web.app",
   },
   {
-    title: "Time, Date & Todo List",
+    title: "Calculator",
     image: "https://via.placeholder.com/150",
     body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequuntur debitis perspiciatis vitae architecto dignissimos quaerat minima ad cumque. Provident.",
+      "A clean, simple calculator app built using class based JavaScript. One of the first full apps built during the course.",
     github: "https://github.com/Zdeevo",
-    weblink: "https://coming-soon-79bfd.web.app",
+    weblink: "https://calculator-63759.web.app/",
   },
 ];
 
