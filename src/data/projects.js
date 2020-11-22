e7240cce24b602a75ac61b00281f64b360c9cb2d
Steve Maguire <dannyblack42@protonmail.com>
2020-11-22 22:47:54 +0000

    more skills/project info, add favicon

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,7 +13,7 @@ const projects = [
     body:
       "A 'choose your own adventure' game utilising JavaScript switch/case to dynamically asisgn attributes with each button click to take you through a magical adventure. Are you brave enough?",
     github: "https://github.com/Zdeevo/hero_quest",
-    weblink: "https://quest.stevemaguire.dev",
+    weblink: "https://heros-choice.web.app/",
   },
   {
     title: "Punk API",
@@ -21,7 +21,7 @@ const projects = [
     body:
       "An app to fetch beers from the Punk API. The list of beers can then be filtered according to strength or searched dynamically using a keyword",
     github: "https://github.com/Zdeevo/punk-beers",
-    weblink: "https://stevemaguire.dev",
+    weblink: "https://coming-soon-79bfd.web.app",
   },
   {
     title: "Kanye Quote Machine",
@@ -29,7 +29,7 @@ const projects = [
     body:
       "Have a chuckle at some of Kanye West's best and most ridiculous quotes, fetched from the Kanye.Rest API.",
     github: "https://github.com/Zdeevo",
-    weblink: "https://stevemaguire.dev",
+    weblink: "https://coming-soon-79bfd.web.app",
   },
   {
     title: "Cocktail Database",
@@ -37,7 +37,7 @@ const projects = [
     body:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequuntur debitis perspiciatis vitae architecto dignissimos quaerat minima ad cumque. Provident.",
     github: "https://github.com/Zdeevo",
-    weblink: "https://stevemaguire.dev",
+    weblink: "https://coming-soon-79bfd.web.app",
   },
   {
     title: "Time, Date & Todo List",
@@ -45,7 +45,7 @@ const projects = [
     body:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequuntur debitis perspiciatis vitae architecto dignissimos quaerat minima ad cumque. Provident.",
     github: "https://github.com/Zdeevo",
-    weblink: "https://stevemaguire.dev",
+    weblink: "https://coming-soon-79bfd.web.app",
   },
 ];
 
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,39 +1,39 @@
 const skills = [
   {
     title: "Frontend Fundamentals",
-    image: "https://i.imgur.com/2o1RvbS.png",
+    image: "https://i.imgur.com/5bglRQZ.png",
     body:
-      "In the early weeks of the course I mastered the fundamnetals of frontend development: HTML, CSS & SCSS (SASS). I learned how to structure and design a web app. I also learned how to take HTML styling to the next level with the CSS extension language, SASS.",
+      "In the early days of the course I mastered the fundamnetals of frontend development: HTML & CSS. I learned how to structure and design a simple web site, as well as taking HTML styling to the next level with the CSS extension language, SASS.",
   },
   {
     title: "JavaScript",
     image: 'https://i.imgur.com/OcduSQV.png',
     body:
-      "I learned this ages ago, I can barely remember, like it was a dream, or a nightmare, still processing it.",
+      "In the second week of the course I learned how to add functionality to web sites with JavaScript (functional and class-based). Using functions, objects, arrays and operators I was able to manage and manipulate data creating dynamic web sites for increased usability. ",
   },
   {
     title: "React",
     image: "https://i.imgur.com/tYnFbuN.png",
     body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequuntur debitis perspiciatis vitae architecto dignissimos quaerat minima ad cumque. Provident.",
+      "In week six I started learning React. The flexiblity provided by this JavaScript library, especially the ability to break an app down into separate components, allows for an even greater level of control and adaptability versus 'vanilla' JS. This portfolio site is written in React.js.",
+  },
+  {
+    title: "Backend",
+    image: "https://i.imgur.com/9WxD8Jq.png",
+    body:
+      "In week 8 of the course I learned how to utilise backend services such as Firestore (Firebase), MongoBD and Heroku. My portfolio site and other projects are all hosted via Firestore.",
   },
   {
     title: "Node.js",
     image: "https://i.imgur.com/JTqQnlK.png",
     body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequuntur debitis perspiciatis vitae architecto dignissimos quaerat minima ad cumque. Provident.",
+      "During week nine of the course I learned how to use Node.js to manage the flow of data between the frontend and backend. My 'Cocktail Databse' app is created using Node.js.",
   },
   {
-    title: "JavaScript",
-    image: "https://via.placeholder.com/150",
+    title: "Other Tech Skills",
+    image: "https://i.imgur.com/czrP19x.png",
     body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequuntur debitis perspiciatis vitae architecto dignissimos quaerat minima ad cumque. Provident.",
-  },
-  {
-    title: "Python",
-    image: "https://i.imgur.com/ZnaBtTu.png",
-    body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequuntur debitis perspiciatis vitae architecto dignissimos quaerat minima ad cumque. Provident.",
+      "During the course I was also introduced to the fundamentals of Typescript, how to utilise unit testing in JS/React, authorization and authentication as well as routing and context.  ",
   },
 ];
 
